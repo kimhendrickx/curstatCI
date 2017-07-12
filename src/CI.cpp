73c0c1f8bee268e028b1ea88dcfeee39c5ef0a4d
pietg <P.Groeneboom@tudelft.nl>
2017-07-12 22:52:08 +0200

    CI project

---
+++ src/CI.cpp
@@ -0,0 +1,451 @@
+#include "curstat.h"
+
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Confidence Intervals
+//
+// Export: Yes
+// Date Created: 11.07.2017
+// Created by: Piet Groeneboom and Kim Hendrickx
+//////////////////////////////////////////////////////////////////////////////////
+//'@title Confidence Intervals
+//'@param input DataFrame with three variables:
+//'\describe{
+//'     \item{xx}{Observation points t sorted in ascending order}
+//'     \item{freq1}{Frequency of observation t satisfying \eqn{y \le t}}
+//'     \item{freq2}{Frequency of observation t}
+//'}
+//'@param x numeric vector
+//'@param alpha level for confidence intervals
+//'@return List with 4 variables:
+//'\enumerate{
+//'     \item{MLE}{ maximum likelihood estimator}
+//'     \item{SMLE}{ smoothed maximum likelihood estimator}
+//'     \item{h}{ local bandwidths for points of grid}
+//'     \item{CI}{ confidence intervals on points of grid}
+//'     }
+//'@export
+// [[Rcpp::export]]
+List ComputeConfIntervals(DataFrame input, NumericVector x, double alpha)
+{
+    double          A,B,*data0,*data,*data1,*data2,*grid,*p,*p2,h;
+    double          *tt,**f3,*lowbound,*upbound,*f4, **MSE, *hmin1;
+    double          *cumw,*cs,*F,*F2,*jumploc,*y,*y2,*SMLEa ,*SMLE,*SMLE2a, *SMLE2,*cc;
+    int             i,j,k,m,m2,N,n,*delta,**freq,njumps,*delta2,*freq1,*freq2,**frequence1;
+    int             percentile1,percentile2,iter,iter2,NumIt=1000,nB, npoints, nIterH;
+    clock_t         StartTime, StopTime;
+    double          Time_bootstrap;
+
+
+    DataFrame DF = Rcpp::DataFrame(input);
+    NumericVector xx = DF["V1"];
+    IntegerVector freq01 = DF["V2"];
+    IntegerVector freq02 = DF["V3"];
+
+    Rcout << std::endl;
+    Rcout << "Piet Groeneboom and Kim Hendrickx 2017" << std::endl << "For further information see:" << std::endl;
+    Rcout << "The nonparametric bootstrap for the current status model," << std::endl;
+    Rcout << "Piet Groeneboom & Kim hendricx, Electronical Journal of Statistics (2017)." << std::endl << std::endl;
+    Rcout << "The program produces the Studentized nonparametric bootstrap confidence intervals for the cdf, using the SMLE" << std::endl<< std::endl << std::endl;
+
+
+    // determine the number of rows of the data frame
+
+    percentile1=(int)((alpha/2)*NumIt);
+    percentile2=(int)((1-alpha/2)*NumIt);
+
+    n = (int)xx.size();
+    npoints = (int)x.size();
+
+
+    data0= new double[n+1];
+    freq1= new int[n+1];
+    freq2= new int[n+1];
+
+    data0[0]=0;
+
+    for (i=1;i<=n;i++)
+    {
+        data0[i]=(double)xx[i-1];
+        freq1[i]=(int)freq01[i-1];
+        freq2[i]=(int)freq02[i-1];
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    A = 0.0;
+    B = data0[n];
+
+
+
+    grid = new double[npoints+1];
+    grid[0] = A;
+
+    for (i=1;i<=npoints;i++)
+        grid[i]= x[i-1];
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    N=0;
+    for (i=1;i<=n;i++)
+        N += freq2[i];
+
+
+    nB = (int) N/2;
+    if(N>= 100 && N < 1000)
+        nB = 50;
+    if(N>= 1000 && N < 10000)
+        nB = 200;
+    if(N>= 10000)
+        nB = 500;
+
+    Rcout << "Number of unique observations:" << std::setw(7) << n << std::endl << std::endl;
+    Rcout << "Sample size N =" << std::setw(5) << N << std::endl;
+    Rcout << "Left point of observation interval A = " <<  std::setw(5) << A << std::endl;
+    Rcout << "Right point of observation interval B = " <<  std::setw(5) << B << std::endl;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // form the data vector for the bootstrap
+
+    data = new double[N+1];
+    delta = new int[N+1];
+    tt=new double[N+1];
+    delta2 = new int[N+1];
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // grid search for optimal bandwidth
+
+    nIterH = 100;
+    cc = new double[nIterH+1];
+    for (i=0;i<=nIterH;i++)
+        cc[i]= 2.5*(B-A)*i/nIterH;
+
+    MSE = new double*[npoints+1];
+    for (iter2=0;iter2<=npoints;iter2++)
+        MSE[iter2] = new double[nIterH+1];
+
+    hmin1 = new double[npoints+1];
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    data[0]=0;
+
+    j=0;
+
+    for (i=1;i<=n;i++)
+    {
+        for (k=1;k<=freq1[i];k++)
+        {
+            j++;
+            data[j]=data0[i];
+            delta[j]=1;
+
+        }
+        for (k=1;k<=freq2[i]-freq1[i];k++)
+        {
+            j++;
+            data[j]=data0[i];
+            delta[j]=0;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    F= new double[n+1];
+    F2= new double[n+1];
+    cumw= new double[n+1];
+    cs= new double[n+1];
+    y= new double[n+1];
+    y2= new double[n+1];
+    jumploc= new double[n+1];
+    data1= new double[n+1];
+    data2= new double[n+1];
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    SMLE= new double[npoints+1];
+    SMLE2= new double[npoints+1];
+    SMLEa= new double[npoints+1];
+    SMLE2a= new double[npoints+1];
+    p= new double[n+1];
+    p2= new double[n+1];
+
+    frequence1 = new int*[2];
+    for (i=0;i<2;i++)
+        frequence1[i] = new int[n+1];
+
+    freq = new int*[2];
+    for (i=0;i<2;i++)
+        freq[i] = new int[n+1];
+
+    f3  = new double*[NumIt+1];
+
+    for (iter=0;iter<NumIt+1;iter++)
+        f3[iter] = new double[npoints];
+
+
+    F[0]=F2[0]=0;
+    cumw[0]=cs[0]=0;
+
+    y[0]=y2[0]=0;
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    StartTime = clock();
+
+    for (i=1;i<=n;i++)
+    {
+        cs[i]=cs[i-1]+(double)freq1[i];
+        cumw[i]=cumw[i-1]+(double)freq2[i];
+        frequence1[1][i]=freq1[i];
+        frequence1[0][i]=freq2[i]-freq1[i];
+    }
+
+
+    convexmin(n,cumw,cs,y);
+
+    j=0;
+
+    jumploc[0]=0;
+
+    for (i=1;i<=n;i++)
+    {
+        if (y[i]>y[i-1])
+        {
+            j++;
+            p[j]=y[i]-y[i-1];
+            F[j]=y[i];
+            jumploc[j]=data0[i];
+        }
+    }
+
+    njumps=j;
+
+    NumericMatrix out1 = NumericMatrix(njumps+1,2);
+
+    for (i=0;i<=njumps;i++)
+    {
+        out1(i,0)=jumploc[i];
+        out1(i,1) = F[i];
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    // bandwidth for initial SMLE
+    h = 2*(B-A)*pow(N,-1.0/5);
+
+    SMLEa[0]=0;
+
+    for (i=1;i<=npoints;i++)
+        SMLEa[i]=bdf(A,B,njumps,jumploc,p,grid[i], h);
+
+
+    for (i=1;i<=npoints;i++)
+    {
+        for (j=1;j<=nIterH;j++)
+        {
+            MSE[i][j]=0;
+        }
+    }
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    for (iter=0;iter<NumIt;iter++)
+    {
+        data_bootstrap2(N,nB,n,&m,data,tt,data1,freq,delta,delta2);
+
+        for (i=1;i<=m;i++)
+        {
+            cs[i]=cs[i-1]+(double)freq[1][i];
+            cumw[i]=cumw[i-1]+(double)(freq[0][i]+freq[1][i]);
+        }
+
+        convexmin(m,cumw,cs,y2);
+
+
+        j=0;
+
+        for (i=1;i<=m;i++)
+        {
+            if (y2[i]>y2[i-1])
+            {
+                j++;
+                data2[j]=data1[i];
+                p2[j]=y2[i]-y2[i-1];
+                F2[j]=y2[i];
+            }
+        }
+
+        m2=j;
+
+
+        SMLE2a[0]=0;
+
+
+        for (i=1;i<=npoints;i++)
+        {
+            for (j=1;j<=nIterH;j++)
+            {
+                SMLE2a[i]=bdf(A,B,m2,data2,p2,grid[i],cc[j]*pow(nB,-1.0/5));
+
+                MSE[i][j] += SQR(SMLE2a[i]-SMLEa[i])/NumIt;
+            }
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Select the optimal bandwidth and bias estimate
+
+    for (i=1;i<=npoints;i++)
+    {
+        hmin1[i]=cc[1]*pow(N,-1.0/4);
+
+        for (j=2;j<=nIterH;j++)
+        {
+            if (MSE[i][j]<MSE[i][j-1])
+                hmin1[i]=cc[j]*pow(N,-1.0/4);
+        }
+        // Rprintf("%16.10f %16.10f %16.10f\n", grid[10*i], hmin1[i], sqrt(varF(N,n,frequence1,y,data0[1],data0[n],data0,hmin1[i],grid[10*i])) );
+    }
+
+
+    // ## Compute SMLE  with optimal bandwidth
+    SMLE[0]=0;
+
+    for (i=1;i<=npoints;i++)
+    {
+        SMLE[i]=bdf(A,B,njumps,jumploc,p,grid[i],hmin1[i]);
+    }
+
+    NumericVector out2(npoints);
+    NumericVector out3(npoints);
+
+    for (i=0;i<npoints;i++)
+    {
+        out2[i] = SMLE[i+1];
+        out3[i] = hmin1[i+1];
+    }
+
+
+    for (iter=0;iter<NumIt;iter++)
+    {
+        data_bootstrap(N,n,&m,data,tt,data1,freq,delta,delta2);
+
+        for (i=1;i<=m;i++)
+        {
+            cs[i]=cs[i-1]+(double)freq[1][i];
+            cumw[i]=cumw[i-1]+(double)(freq[0][i]+freq[1][i]);
+        }
+
+        convexmin(m,cumw,cs,y2);
+
+
+        j=0;
+
+        for (i=1;i<=m;i++)
+        {
+            if (y2[i]>y2[i-1])
+            {
+                j++;
+                data2[j]=data1[i];
+                p2[j]=y2[i]-y2[i-1];
+                F2[j]=y2[i];
+            }
+        }
+
+        m2=j;
+
+
+        SMLE2[0]=0;
+
+
+        for(i =1; i<=npoints; i++)
+        {
+            SMLE2[i]=bdf(A,B,m2,data2,p2,grid[i],hmin1[i]);
+
+            if(sqrt(varF(N,n,frequence1,y,data0[1],data0[n],data0,hmin1[i],grid[i])) > 0)
+            {
+                f3[iter][i]=(SMLE2[i]-SMLE[i])/( (sqrt( varF(N,m,freq,y2,0.0,B,data1,hmin1[i],grid[i]))));
+            }
+            else{
+                f3[iter][i]=(SMLE2[i]-SMLE[i])/( (sqrt( varF(N,m,freq,y2,0.0,B,data1,(B-A)/2,grid[i]))));
+            }
+
+        }
+
+
+
+    }
+
+
+    StopTime  = clock();
+    Time_bootstrap   = (double)(StopTime - StartTime)/(double)CLOCKS_PER_SEC;
+
+    f4= new double[NumIt+1];
+
+    lowbound=new double[npoints];
+    upbound=new double[npoints];
+
+    for (i=1;i<=npoints;i++)
+    {
+        for (iter=0;iter<NumIt;iter++)
+            f4[iter]=f3[iter][i];
+
+        qsort(f4,NumIt,sizeof(double),compare);
+
+        lowbound[i]= fmax(0,SMLE[i]-f4[percentile2-1]*sqrt(varF(N,n,frequence1,y,data0[1],data0[n],data0,hmin1[i],grid[i])));;
+        upbound[i]= fmin(1,SMLE[i]-f4[percentile1-1]*sqrt(varF(N,n,frequence1,y,data0[1],data0[n],data0,hmin1[i],grid[i])));;
+    }
+
+    Rcout << std::endl << std::endl;
+    Rcout << "The computations took    " << setprecision(10) << Time_bootstrap << "   seconds"  << std::endl;
+
+    NumericMatrix out4 = NumericMatrix(npoints,2);
+
+    for (i=0;i<npoints;i++)
+    {
+        out4(i,0)=lowbound[i+1];
+        out4(i,1)=upbound[i+1];
+    }
+
+    Rcout << std::endl;
+
+    //ofstream file_("/Users/pietgroeneboom/Documents/curstat_package/CI_SMLE.txt");
+    ofstream file_("CI_SMLE.txt");
+
+    if (file_.is_open())
+    {
+        for (i=1;i<npoints;i++)
+        {
+            file_ << setprecision(10) << setw(20) << grid[i];
+            file_ << setprecision(11) <<  setw(20) << lowbound[i]
+            << setprecision(11) <<  setw(20) << upbound[i];
+            file_ << "\n";
+        }
+        file_.close();
+    }
+
+
+
+    // make the list for the output, containing the MLE, hazard, the bootstrap confidence intervals and -log likelihood
+
+    List out = List::create(Rcpp::Named("MLE")=out1,Rcpp::Named("SMLE")=out2,Rcpp::Named("h")=out3,Rcpp::Named("CI")=out4 );
+
+    // free memory
+
+    delete[] data, delete[] delta, delete[] tt, delete[] delta2, delete[] SMLE, delete[] SMLE2,
+    delete[] F, delete[] F2, delete[] cumw, delete[] cs, delete[] y, delete[] y2,
+    delete[] jumploc,  delete[] data0, delete[] data1, delete[] data2, delete[] p, delete[] p2,
+    delete[] lowbound, delete[] upbound;
+
+    for (i = 0;i<2;i++)
+        delete[] freq[i];
+    delete[] freq;
+
+    for (iter = 0;iter < NumIt;iter++)
+        delete[] f3[iter];
+    delete[] f3;
+
+    return out;
+}
---
+++ src/CurrentStatus_Functions.cpp
@@ -0,0 +1,226 @@
+#include "curstat.h"
+
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Maximum Likellood Estimator (MLE)
+//
+// Export: Yes
+// Date Created: 07.07.2017
+// Created by: Kim Hendrickx
+//////////////////////////////////////////////////////////////////////////////////
+//'@title Maximum Likelihood Estimator
+//'@param data DataFrame with three variables:
+//'\describe{
+//'     \item{t}{Observation points t sorted in ascending order}
+//'     \item{freq1}{Frequency of observation t satisfying \eqn{y \le t}}
+//'     \item{freq2}{Frequency of observation t}
+//'}
+//'@return DataFrame with two variables :
+//'\describe{
+//'     \item{x}{jumplocations of the MLE}
+//'     \item{mle}{MLE evaluated at the jumplocations}
+//' }
+//'@export
+// [[Rcpp::export]]
+DataFrame ComputeMLE(DataFrame data)
+{
+  double          *data0,*cumw,*cs,*F,*jumploc,*y;
+  int             i,j,n,njumps,*freq1,*freq2;
+
+  DataFrame DF = Rcpp::DataFrame(data);
+  NumericVector xx = DF["V1"];
+  IntegerVector freq01 = DF["V2"];
+  IntegerVector freq02 = DF["V3"];
+
+  n = (int)xx.size();
+
+  data0= new double[n+1];
+  freq1= new int[n+1];
+  freq2= new int[n+1];
+
+  data0[0]=0;
+
+  for (i=1;i<=n;i++)
+  {
+    data0[i]=(double)xx[i-1];
+    freq1[i]=(int)freq01[i-1];
+    freq2[i]=(int)freq02[i-1];
+  }
+
+  F= new double[n+1];
+  cumw= new double[n+1];
+  cs= new double[n+1];
+  y= new double[n+1];
+  jumploc= new double[n+1];
+
+
+  F[0]=0;
+  cumw[0]=cs[0]=0;
+
+  y[0]=0;
+
+  for (i=1;i<=n;i++)
+  {
+    cs[i]=cs[i-1]+(double)freq1[i];
+    cumw[i]=cumw[i-1]+(double)freq2[i];
+  }
+
+  convexmin(n,cumw,cs,y);
+
+  j=0;
+
+  jumploc[0]=0;
+
+  for (i=1;i<=n;i++)
+  {
+    if (y[i]>y[i-1])
+    {
+      j++;
+      F[j]=y[i];
+      jumploc[j]=data0[i];
+    }
+  }
+
+  njumps=j;
+
+  NumericVector out1(njumps+1);
+  NumericVector out2(njumps+1);
+
+  for (i=0;i<=njumps;i++)
+  {
+    out1[i]=jumploc[i];
+    out2[i] = F[i];
+  }
+
+
+  DataFrame out = DataFrame::create(Rcpp::Named("x")=out1, Rcpp::Named("mle")=out2);
+
+
+  // free memory
+
+  delete[] F, delete[] cumw, delete[] cs, delete[] y,
+  delete[] jumploc,  delete[] data0;
+  delete[] freq1; delete[] freq2;
+
+  return out;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Smoothed Maximum Likellood Estimator (SMLE)
+//
+// Export: Yes
+// Date Created: 07.07.2017
+// Created by: Kim Hendrickx
+//////////////////////////////////////////////////////////////////////////////////
+
+//'@title Smoothed Maximum Likelihood Estimator
+//'@param data DataFrame with three variables:
+//'\describe{
+//'     \item{t}{Observation points t sorted in ascending order}
+//'     \item{freq1}{Frequency of observation t satisfying \eqn{y \le t}}
+//'     \item{freq2}{Frequency of observation t}
+//'}
+//'@param h bandwidth
+//'@param x a number or vector
+//'@return SMLE(x)
+//'
+//'@export
+// [[Rcpp::export]]
+NumericVector ComputeSMLE(DataFrame data, NumericVector x, double h)
+{
+  double          *data0,*cumw,*cs,*F,*p,*jumploc,*y,*grid,A,B;
+  int             i,j,k,n,ngrid,njumps,*freq1,*freq2;
+
+  DataFrame DF = Rcpp::DataFrame(data);
+  NumericVector xx = DF["V1"];
+  IntegerVector freq01 = DF["V2"];
+  IntegerVector freq02 = DF["V3"];
+
+  n = (int)xx.size();
+  ngrid = (int)x.size();
+
+  data0= new double[n+1];
+  freq1= new int[n+1];
+  freq2= new int[n+1];
+
+  data0[0]=0;
+
+  for (i=1;i<=n;i++)
+  {
+    data0[i]=(double)xx[i-1];
+    freq1[i]=(int)freq01[i-1];
+    freq2[i]=(int)freq02[i-1];
+  }
+
+  A = 0.0;
+  B = data0[n];
+
+  F= new double[n+1];
+  p= new double[n+1];
+  cumw= new double[n+1];
+  cs= new double[n+1];
+  y= new double[n+1];
+  jumploc= new double[n+1];
+  grid= new double[ngrid];
+
+  for (i=0;i<ngrid;i++)
+    grid[i]=(double)x[i];
+
+
+  F[0]=0;
+  cumw[0]=cs[0]=0;
+
+  y[0]=0;
+
+  for (i=1;i<=n;i++)
+  {
+    cs[i]=cs[i-1]+(double)freq1[i];
+    cumw[i]=cumw[i-1]+(double)freq2[i];
+  }
+
+  convexmin(n,cumw,cs,y);
+
+  j=0;
+
+  jumploc[0]=0;
+
+  for (i=1;i<=n;i++)
+  {
+    if (y[i]>y[i-1])
+    {
+      j++;
+      p[j]=y[i]-y[i-1];
+      F[j]=y[i];
+      jumploc[j]=data0[i];
+    }
+  }
+
+  njumps=j;
+
+  double sum, t1,t2,t3;
+  NumericVector out(ngrid);
+
+    for(i=0; i<ngrid;i++)
+    {
+      sum=0;
+      for (k=1;k<=njumps;k++)
+        {
+        t1=(grid[i]-jumploc[k])/h;
+        t2=(grid[i]+jumploc[k]-2*A)/h;
+        t3=(2*B-grid[i]-jumploc[k])/h;
+        sum+= (KK(t1)+KK(t2)-KK(t3))*p[k];
+        }
+      out[i] =fmax(0,sum) ;
+    }
+
+
+    // free memory
+
+    delete[] F, delete[] cumw, delete[] cs, delete[] y, delete[] p;
+    delete[] jumploc,  delete[] data0, delete[] grid;
+    delete[] freq1; delete[] freq2;
+
+    return out;
+
+}
